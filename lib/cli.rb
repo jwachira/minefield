4ef40926930d9fae08e45b6ac83a2bdd86d9169e
James Wachira <jamomathenge@gmail.com>
2017-08-31 01:29:43 -0700

    -fix broken explosion calc specs -add some puts to show progress

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,14 +3,18 @@ class CLI
     input_path  = ARGV.shift || './input.txt'
     output_path = ARGV.shift || './output.txt'
 
+    puts "Importing mine cordinates from #{input_path}".freeze
     mine_coordinates = InputReader.new(input_path).read
 
+    puts "Creating mine entries....".freeze
     mines   = mine_coordinates.map do |coordinates|
                 Mine.new(*coordinates)
               end
 
+    puts "Explode those mines!!!".freeze
     results = ExplosionCalculator.new(mines).explode
 
+    puts "Write explosion counters into a file...".freeze
     File.open(output_path, 'w') do |file|
       file.write "Mine | Explosion Counter\n"
 
@@ -18,5 +22,7 @@ class CLI
         file.write format("%s | %s\n", result.mine.to_s, result.explosion_counter)
       end
     end
+
+    puts "Done! You can find the explosion report at #{output_path}"
   end
 end
